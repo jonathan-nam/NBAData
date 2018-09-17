432f68492051e9467647e2ae5969691c71fdb335
Jonathan <jonathan.nam14@gmail.com>
2018-09-16 23:29:35 -0400

    code reorganization, additions to Team

--- NBAData/src/basicRetrieval/Team.java
+++ NBAData/src/basicRetrieval/Team.java
@@ -1,12 +1,21 @@
 package basicRetrieval;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 public class Team {
+	private static final int NUM_PLAYERS = 15;
 	private String name;
 	private String [] players;
+	private String teamLink;
 	
-	public Team (String name, String [] players) {
+	public Team (String teamLink, String name) throws IOException {
+		this.teamLink = teamLink;
 		this.name = name;
-		this.players = players;
+		initTeam();
 	}
 	
 	public String getName() {
@@ -15,10 +24,33 @@ public class Team {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String[] getPlayers() {
 		return players;
 	}
+	
 	public void setPlayers(String[] players) {
 		this.players = players;
+	}
+	
+	public void initTeam() throws IOException {
+		Document teamConn = Jsoup.connect(teamLink).ignoreHttpErrors(true).get();
+		Element refinedDoc = teamConn.body();
+		
+		System.out.println(refinedDoc.toString());
+	}
+	
+	public StringBuilder printPlayers() {
+		StringBuilder allPlayers = new StringBuilder("");
+		
+		for (int i = 0; i < players.length; i++) {
+			allPlayers.append(players[i] + "\n");
+		}
+		
+		return allPlayers;
+	}
+	
+	public String toString() {
+		return "Name: " + name + "\nURL: " + teamLink + "\n"; 
 	}
 }
--- NBAData/src/basicRetrieval/dataScrape.java
+++ NBAData/src/basicRetrieval/dataScrape.java
@@ -1,53 +1,43 @@
 package basicRetrieval;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.*;
 import org.jsoup.select.Elements;
 
 public class dataScrape {
+	private static final String year = "2019.html";
 	private static final int NUM_TEAMS = 30;
-	private static final int NUM_PLAYERS = 15;
-	private static String [][] allTeams = new String [NUM_TEAMS][NUM_PLAYERS];
 	private static String url = "https://www.basketball-reference.com/teams/";
-	private static String teamSelector = 
-			"div[id=all_teams_active] [data-stat^=franch_name] > a[href^=/teams/]";
+	private static ArrayList <Team> teams = new ArrayList<Team>(NUM_TEAMS);
 	
 	public static void main (String [] args) throws IOException {
+		init();
+	}
+	
+	private static void init() throws IOException {	
+		String teamSelector = 
+				"div[id=all_teams_active] [data-stat^=franch_name] > a[href^=/teams/]";
+		
+		/* Start */
 		Document connection = Jsoup.connect(url).get();
 		Element refinedDoc = connection.body();
-		//System.out.println(refinedDoc.toString());
-				
-		Elements teamNames = refinedDoc.select(teamSelector);
-		
-		for (int i = 0; i < teamNames.size(); i++) {
-			allTeams[i][0] = teamNames.get(i).text();
-			System.out.println(teamNames.get(i));
+		System.out.println(refinedDoc.toString());
 			
-			String teamLink = teamNames.get(i).absUrl("href") + "2019.html";
-			System.out.println("LINK: " + teamLink);
-			addPlayers(teamLink, i);
+		Elements teamNames = refinedDoc.select(teamSelector);	
+		
+		/* initialize list of teams */
+		int count = 0;
+		for (Element teamName : teamNames) {
+			String teamLink = teamName.absUrl("href") + year;
+			String name = teamName.text();
+			
+			Team team = new Team(teamLink, name);
+			teams.add(count, team);
+			count++;
 		}
-		
-		//printArray(allTeams);
-	}
-	
-	private static void addPlayers(String teamUrl, int i) throws IOException {
-		Document teamConn = Jsoup.connect(teamUrl).ignoreHttpErrors(true).get();
-		
-		System.out.println("URL: " + teamUrl + "\nindex: " + i);
-		//System.out.println(refinedDoc.toString());
-		
-	}
-//	-----------------HELPER METHODS-----------------
-	
-	private static void printArray(String [][] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[i].length; j++) {
-				System.out.print(arr[i][j] + " ");
-			}
-			System.out.println();
-		}
+		System.out.println(teams);
 	}
 }
