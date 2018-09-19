e29b847a0e8e8c3e2a0943379909975dfb11b144
Jonathan <jonathan.nam14@gmail.com>
2018-09-18 23:00:43 -0400

    additions to Player

--- NBAData/src/basicRetrieval/Player.java
+++ NBAData/src/basicRetrieval/Player.java
@@ -1,5 +1,25 @@
 package basicRetrieval;
 
+import java.util.HashMap;
+
 public class Player {
- 
+	private String playerLink;
+	private String name;
+	private HashMap<String, String> stats;
+	
+	public String getPlayerLink() {
+		return playerLink;
+	}
+	
+	public void setPlayerLink(String playerLink) {
+		this.playerLink = playerLink;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 }
